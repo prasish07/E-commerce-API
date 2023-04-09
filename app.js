4ec0bf6583c5958f2a9cf5c4258ce41f96dd4a74
prasish <prasishshrestha0099@gmail.com>
2023-04-09 08:12:52 +0545

    added few fixes

--- app.js
+++ app.js
@@ -37,18 +37,10 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSenitize());
 
-app.use(morgon("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./Public"));
 app.use(fileUpload());
-
-//get request
-app.get("/api/v1", (req, res) => {
-  // console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.send("hello");
-});
 
 // routes
 app.use("/api/v1/auth", authrouter);
